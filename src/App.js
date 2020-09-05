942c6bf35c21c701558e9c529c0b9896462cdd7f
madhuvayya <madhu.vayya@gmail.com>
2020-09-05 23:48:53 +0530

    Added code to get the response ith get request

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import UserMessage from './components/UserMessage';
 import UsersComponent from './components/UsersComponent'
 import Content from './components/Content'
+import GetRequest from './components/GetRequest'
 
 function App() {
   return (
     <div className="App">
       {/* <UserMessage /> */}
       {/* <UsersComponent /> */}
-      <Content />
+      {/* <Content /> */}
+      <GetRequest />
     </div>
   );
 }
